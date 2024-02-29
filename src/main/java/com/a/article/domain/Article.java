2640a58995534cce6049d52a95a58aa162226708
kuseulki <kuseulki@naver.com>
2024-03-01 00:28:16 +0900

    #5 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1 테이블 매칭되도록 설계

--- src/main/java/com/a/article/domain/Article.java
+++ src/main/java/com/a/article/domain/Article.java
@@ -1,17 +1,81 @@
 package com.a.article.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString(callSuper = true)
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private String content;
-    private String hashtag;
 
+    @Setter @Column(nullable = false)
+    private String title;
+
+    @Setter @Column(nullable = false, length = 10000)
+    private String content;
+
+    @Setter private String hashtag;
+
+    @ToString.Exclude
+    @OrderBy("createdAt DESC")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+
+    @CreatedDate @Column(nullable = false, updatable = false)
     protected LocalDateTime createdAt; // 생성일시
+
+    @CreatedBy @Column(nullable = false, updatable = false, length = 100)
     protected String createdBy; // 생성자
+
+    @LastModifiedDate @Column(nullable = false)
     protected LocalDateTime modifiedAt; // 수정일시
+
+    @LastModifiedBy @Column(nullable = false, length = 100)
     protected String modifiedBy; // 수정자
 
+    protected Article() {}
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article that)) return false;
+        return this.getId() != null && this.getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
