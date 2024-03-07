ce337574484032399a29192868448be77a1f366e
kuseulki <kuseulki@naver.com>
2024-03-08 00:16:50 +0900

    #31 - 해시태그

---
+++ src/main/java/com/a/article/repository/querydsl/ArticleRepositoryCustomImpl.java
@@ -0,0 +1,26 @@
+package com.a.article.repository.querydsl;
+
+import com.a.article.domain.Article;
+import com.a.article.domain.QArticle;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class ArticleRepositoryCustomImpl extends QuerydslRepositorySupport implements ArticleRepositoryCustom {
+
+    public ArticleRepositoryCustomImpl() {
+        super(Article.class);
+    }
+
+    @Override
+    public List<String> findAllDistinctHashtags() {
+        QArticle article = QArticle.article;
+
+        return from(article)
+                .distinct()
+                .select(article.hashtag)
+                .where(article.hashtag.isNotNull())
+                .fetch();
+    }
+
+}
